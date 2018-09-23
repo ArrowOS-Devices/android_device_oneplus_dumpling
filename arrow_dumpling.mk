c5f304afc4c571bb7bc3ec5649f7016e642d499e
Bauuuuu <abansode@outlook.com>
2018-09-23 22:04:57 +0800

    arrow bringup

--- arrow_dumpling.mk
+++ arrow_dumpling.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
-# Inherit some common gzosp stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+# Inherit some common arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := gzosp_dumpling
+PRODUCT_NAME := arrow_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
